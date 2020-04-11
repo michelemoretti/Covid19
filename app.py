93890bc612b55501b0dc57ea4d571acd4bf216d5
Michele Moretti <cicobalico88@gmail.com>
2020-04-11 16:52:08 +0200

    Dynamically adjust top values

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import logging
 import math
 import os
 from datetime import date, datetime
+import locale
 
 import dash
 import dash_core_components as dcc
@@ -14,11 +15,20 @@ from dash.dependencies import Input, Output
 
 from figures import get_regional_map
 from utils import (
-    calcolo_giorni_da_min_positivi, calculate_line, exp_viridis, get_areas,
-    get_dataset, get_map_json, linear_reg, mean_absolute_percentage_error,
-    pretty_colors, viridis)
+    calcolo_giorni_da_min_positivi,
+    calculate_line,
+    exp_viridis,
+    get_areas,
+    get_dataset,
+    get_map_json,
+    linear_reg,
+    mean_absolute_percentage_error,
+    pretty_colors,
+    viridis,
+)
 
-logger = logging.getLogger('dash_application')
+locale.setlocale(locale.LC_ALL, '')
+logger = logging.getLogger("dash_application")
 logger.setLevel(logging.DEBUG)
 
 # external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css",os.path.join("assets","dashboard.css")]
@@ -31,6 +41,12 @@ regions, provinces = get_areas(df)
 giorni_da_min_positivi = calcolo_giorni_da_min_positivi(df_regioni)
 viridis_exp_scale = exp_viridis(giorni_da_min_positivi)
 
+metric_names = {"totale_casi":"Contagiati",
+                "deceduti": "Deceduti",
+                "tamponi":"Tamponi",
+                "terapia_intensiva":"Terapia Intensiva",
+                }
+metric_list = ["totale_casi", "deceduti", "tamponi", "terapia_intensiva"]
 
 def get_top_bar(metrics_list, area="IT", day=datetime.today):
 
@@ -46,19 +62,19 @@ def get_top_bar(metrics_list, area="IT", day=datetime.today):
                         className="big-metric-number",
                     ),
                     html.Div(
-                        metric, id=f"daily-{metric}-label", className="big-metric-label"
+                        metric_names[metric], id=f"daily-{metric}-label", className="big-metric-label"
                     ),
                 ],
                 id=f"daily_{metric}",
                 className="big-metric",
             )
         )
-    return html.Div(top_metrics, id="top-bar", className="dashboardContainer")
+    return html.Div(top_metrics, id="top-bar", className="row dashboardContainer")
 
 
 app.layout = html.Div(
     [
-        get_top_bar(["Contagiati", "Deceduti", "Tamponi", "Terapia Intensiva"]),
+        get_top_bar(metric_list),
         html.Div(
             dcc.Graph(
                 id="main-map",
@@ -68,11 +84,14 @@ app.layout = html.Div(
                     viridis_exp_scale,
                     "totale_casi",
                     "Totale Casi",
-                    max_scale_value=math.ceil(df_regioni["totale_casi"].max()+1),
+                    max_scale_value=math.ceil(df_regioni["totale_casi"].max() + 1),
                 ),
+                # animate=True,
                 # config={"displayModeBar": False}),
                 className="dashboardContainer",
             ),
+            id="center-row",
+            className="row"
         ),
         html.Div(
             [
@@ -83,33 +102,40 @@ app.layout = html.Div(
                     marks={
                         giorno.toordinal(): {
                             "label": f"{str(giorno.day).zfill(2)}/{str(giorno.month).zfill(2)}/{str(giorno.year)[:2]}",
-                            "style": {"color": "#444",
-
-                                    "transform":"rotate(45deg)"},
+                            "style": {"color": "#444", "transform": "rotate(45deg)"},
                         }
                         for giorno in df["data"].unique()
                     },
                     id="date-slider",
                 ),
             ],
-        )
+        ),
     ]
 )
 
 
-@app.callback(Output("main-map", component_property="figure"), [Input("date-slider", "value"),])
+@app.callback(
+    [Output("main-map", component_property="figure")]
+    + [
+        Output(f"daily-{metric}-number", component_property="children")
+        for metric in metric_list
+    ],
+    [Input("date-slider", "value"),],
+)
 def update_map(ordinal_date):
-    giorno = date.fromordinal(ordinal_date)    
-
+    giorno = date.fromordinal(ordinal_date)
+    filtered_df = df_regioni[df_regioni["data"].dt.date == giorno]
+    # logger.debug()
     figure = get_regional_map(
-                    df_regioni[df_regioni["data"].dt.date==giorno],
-                    regions_map_json,
-                    viridis_exp_scale,
-                    "totale_casi",
-                    "Totale Casi",
-                    math.ceil(df_regioni["totale_casi"].max()+1)
-                )
-    return figure
+        filtered_df,
+        regions_map_json,
+        viridis_exp_scale,
+        "totale_casi",
+        "Totale Casi",
+        math.ceil(df_regioni["totale_casi"].max() + 1),
+    )
+    response_number = [f"{filtered_df[metric].sum():n}" for metric in metric_list]
+    return [figure] + response_number
 
 
 if __name__ == "__main__":
