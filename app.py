d166ab233faa85ae46c125061ebfc79f8a138008
Michele Moretti <michele@neurality.it>
2020-04-24 21:07:54 +0200

    Analytics

--- app.py
+++ app.py
@@ -182,16 +182,13 @@ cache = Cache(app.server, config=cache_config)
 app.index_string = """<!DOCTYPE html>
 <html>
     <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162843377-2"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-162843377-2');
-        gtag('config', 'GA_TRACKING_ID', { 'anonymize_ip': true });
-
-        </script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M7SKBTJ');</script>
+        <!-- End Google Tag Manager -->
 
         {%metas%}
         <title>{%title%}</title>
@@ -199,6 +196,10 @@ app.index_string = """<!DOCTYPE html>
         {%css%}
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7SKBTJ"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         {%app_entry%}
         <footer>
             {%config%}
@@ -640,6 +641,7 @@ app.layout = dfx.Grid(
                         html.P(
                             "Click su una regione per selezionarla",
                             className="tooltip-map-p",
+                            id="map-tooltip",
                         ),
                         html.P(
                             "Shift+Click su più regioni per selezionarle",
@@ -658,6 +660,7 @@ app.layout = dfx.Grid(
                     target=f"map-info-button",
                     placement="top",
                     className="tooltip-map",
+                    
                 ),
                 dbc.Tooltip(
                     [
